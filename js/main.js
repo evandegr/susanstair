24bb668fa5f3c701d9c11cc58716eafe8ef34937
Evan Vandegriff <evandegr@gmail.com>
2012-09-22 17:26:57 -0400

    adding file structure

--- js/main.js
+++ js/main.js
@@ -8,22 +8,46 @@
 	function initCarousel(){
 		var $templates = $('#templates');
 		
-		$templates.load('templates/slideshow.html',fillCarousel);
+		$.get('templates/slideshow.html',function(data){
+			$templates.append($(data));
+			fillCarousel('../json/rockfaces.json');
+		});
 	}
-	
-	
+
 	function fillCarousel(jsonFileName){
 		var $templates = $('#templates');
 		
-		function getAllSupportedItems( ) {
-		    return $.getJSON(jsonFileName).pipe(function (data) {
-		        return data.items;
-		    });
-		}
-
-		// Usage:
-		getAllSupportedItems.done(function (items) {
-		    // you have your items here
-		});
+		getCarouselData(jsonFileName);
 	}
+	
+	function getCarouselData(jsonFileName) {
+	    $.getJSON(jsonFileName, function (data) {
+	        buildCarousel(data);
+	        return data;
+	    });
+	}
+	
+	function buildCarousel (data){
+		var $templates = $('#templates'),
+		$carousel = $templates.find('#myCarousel'),
+		$item = $templates.find('.item').clone();
+		
+		$carousel.find('.item').remove();
+		$.each(data,function(){
+			var $itemClone = $item.clone();
+			
+			$itemClone.find('.image').attr('src', this.src);
+			$itemClone.find('.image').data('id', this.id);
+			$itemClone.find('.thumbnail-label').text(this.label);
+			$itemClone.find('.description').text(this.description);
+			
+			$carousel.find('.carousel-inner').prepend($itemClone);
+		});
+
+		$('.viewer').append($carousel);
+		
+		$carousel.carousel();
+
+	}
+
 })();
